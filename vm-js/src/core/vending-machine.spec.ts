808816a19412f433eeac1982f462473b3dbd7172
Trish Jedrick Coyoca <trish.jedrick@school.gsatech.com.sg>
2022-05-26 09:51:05 +0800

    - Added initial test suite

---
+++ vm-js/src/core/vending-machine.spec.ts
@@ -0,0 +1,5 @@
+describe("Vending Machine", () => {
+  it("should not accept amount not listed in denominations", () => {});
+  it("should not accept invalid currency", () => {});
+  it("should not vend if money is insufficient", () => {});
+});
